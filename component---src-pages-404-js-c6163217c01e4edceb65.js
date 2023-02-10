a18ddb1a9b1a2e7d3486fa96c1e9c6f75f30407e
bilal ashraf <bilal.ashraf@codefulcrum.com>
2023-02-11 01:58:09 +0500

    new version

---
+++ component---src-pages-404-js-c6163217c01e4edceb65.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[8883],{429:function(e,t,n){n.r(t);var a=n(1597),r=n(7294);t.default=function(){var e=(0,r.useState)(!1),t=e[0],n=e[1];if((0,r.useEffect)((function(){n(!0),(0,a.navigate)("/not-found")}),[]),t)return r.createElement("div",null,"   ")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-c6163217c01e4edceb65.js.map
